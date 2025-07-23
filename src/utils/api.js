b8d624a9636b87d106c74f60dfb287cdf7a7debe
Nico Rapelas <nicorapelas@gmail.com>
2025-07-23 12:07:58 +0200

    base url update

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,10 +2,20 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' 
-    ? '/api' 
-    : 'https://nuke-brand-d2ea8b9b3c96.herokuapp.com/api',
-  timeout: 10000,
+  baseURL: 'https://nuke-brand-d2ea8b9b3c96.herokuapp.com/api'
 });
 
 export default api; 
+
+// import axios from 'axios';
+
+// // Create axios instance with base URL
+// const api = axios.create({
+//   baseURL: process.env.NODE_ENV === 'production' 
+//     ? '/api' 
+//     : 'https://nuke-brand-d2ea8b9b3c96.herokuapp.com/api',
+//   timeout: 10000,
+// });
+
+// export default api; 
+
